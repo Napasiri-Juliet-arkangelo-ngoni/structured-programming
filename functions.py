5bfa461ab3f7a11baadda4b03722c3bc63303af7
Napasiri-Juliet-arkangelo-ngoni <julietarkangelo@gmail.com>
2025-06-19 09:44:01 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,82 @@
+#
+#
+# f(x) = x + 5
+# f(1) = 1 + 5 = 6
+# f(2) = 2 + 5 = 7
+# f(4) = 4 + 5 = 9
+# f(5) = 5 + 5 = 10
+
+
+# f(x,y) = x + y
+# f(2,1) = 2 + 1 = 3
+# f(4,8) = 4 + 8 = 12
+#
+
+
+user_name = "Abiko Resty"
+print(f"Good morning, {user_name}🖐️")
+
+user_name = "Napasiri Juliet"
+print(f"Good morning, {user_name}🖐️")
+
+user_name = "Abiko Resty"
+print(f"Good evening, {user_name}🌥️")
+
+user_name = "Imelda Charles"
+print(f"Good evening, {user_name}🌥️")
+
+# Tabnine | Edit | Test | Explain | Document
+def function_name():
+    return "anything you want"
+# print(function_name)
+
+
+# Tabnine | Edit | Test | Explain | Document
+def greet_user (name):
+    print(f"Good morning, {name}")
+
+
+input("What is your name?")
+
+
+x = 7
+new_x = int(x)
+new_x = float(x)
+new_x = str(x)
+
+
+
+# print("Something", end"\t")
+print("Something2", "Something1", "Something4", sep )
+
+greet_user("Jane")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
