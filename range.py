aa8a8638c733800d749eec5a9ce28f7592f0b67a
Napasiri-Juliet-arkangelo-ngoni <julietarkangelo@gmail.com>
2025-07-11 12:24:49 +0300

    Add files via upload

---
+++ range.py
@@ -0,0 +1,22 @@
+i = int(input("Please enter a positive number"))
+total = 0
+
+if i < 0:
+    print("Please enter a positive number")
+
+total = 0
+for i in range(0, i+1):
+    if i%2 ==0:
+        total = total + i
+        print(i)
+
+#ist_of_numbers = [1,2,3,4,5,6,7,8,9,10]
+
+#for number in list_of_numbers:
+ #   print(number)
+
+shopping_list = ["soap","bread", "sugar","coffee", "noodles"]
+
+# print(shopping_list[0])
+# print(shopping_list[1])
+# print(shopping_list[2])
