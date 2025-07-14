e0417991b14197322ad729621e34a4f97c78579d
Napasiri-Juliet-arkangelo-ngoni <julietarkangelo@gmail.com>
2025-07-14 17:42:52 +0300

    Add files via upload

---
+++ perimeter.py
@@ -0,0 +1,13 @@
+# Input length and width from user
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate area
+area = length * width
+
+# Calculate perimeter
+perimeter = 2 * (length + width)
+
+# Display results
+print("The area of the rectangle is:", area)
+print("The perimeter (length around) of the rectangle is:", perimeter)
---
+++ perimeter_of _a_square.py
@@ -0,0 +1,8 @@
+# Input length from user
+length = float(input("Enter the length of the square: "))
+
+# Calculate perimeter
+perimeter = 4 * (length)
+
+# Display results
+print("The perimeter (length around) of the square is:", perimeter)
