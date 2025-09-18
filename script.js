3fcd157d99c94fd22075456b68ae9fe7a84c2625
Napasiri-Juliet-arkangelo-ngoni <julietarkangelo@gmail.com>
2025-09-18 15:20:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//let course_work_mark = 8;
+//let exam_mark = 6;
+//const myFirstName = "Juliet"
+//let FinalMark = course_work_mark + exam_mark;
+//console.log("The Final Score is:" + FinalMark)
+const surname = "Manvic";
+const other_name = "Victor";
+const Full_name = surname + other_name;
+console.log("Your full name is:" + Full_name)
+
+
+
+
+
+
+
+
