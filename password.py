346404f1d7c9afc84e0451ee31e34e4dfc338db2
Napasiri-Juliet-arkangelo-ngoni <julietarkangelo@gmail.com>
2025-09-22 17:34:57 +0300

    Add files via upload

---
+++ password.py
@@ -0,0 +1,18 @@
+USERNAME = "Juliet"
+PASSWORD = "mypassword123"
+
+# Allow 3 attempts
+attempts = 3
+while attempts > 0:
+    user = input("Enter username: ")
+    pwd = input("Enter password: ")
+
+    if user == USERNAME and pwd == PASSWORD:
+        print("✅ Login successful! Welcome,", user)
+        break
+    else:
+        attempts -= 1
+        print(f"❌ Incorrect username or password. Attempts left: {attempts}")
+
+if attempts == 0:
+    print("🚫 Too many failed attempts. Access denied!")
