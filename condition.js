9b4730251bb5b9d3c900acbd50c56b1b62bcccd9
Napasiri-Juliet-arkangelo-ngoni <julietarkangelo@gmail.com>
2025-09-25 14:48:18 +0300

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,25 @@
+let coursemarks = 35;
+
+let exammark = 30;
+
+let finalmark = coursemarks + exammark ;
+
+if (finalmark < 50) {
+    console.log("You have a retake");
+} else {
+    console.log("You have successfully passed");
+}
+
+// Range (0 - 49 F) (50 - 59 C) (60 - 69 B) (70 AND ABOVE A)
+if (finalmark <= 49) {
+    console.log("GRADE F");
+     if (finalmark == 0) {
+        console.log("You should repeat")
+    }
+} else if ( 50 <= finalmark <= 59) {
+
+} else if ( 60 <= finalmark <= 69) {
+    console.log("GRADE B")
+} else {
+    console.log("GRADE A")
+}
